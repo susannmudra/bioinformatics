ea8878e3fe91b89fbb226d69b925724310b455b2
susannmudra <83207982+susannmudra@users.noreply.github.com>
2022-07-22 17:56:02 +0200

    Create frequencyMap.py
    The number of k-mers stored in a dictionary.

---
+++ frequencyMap.py
@@ -0,0 +1,16 @@
+def FrequencyMap(Text, k):
+    freq = {} 
+    n = len(Text) 
+    for i in range(n-k+1): 
+        Pattern = Text[i:i+k]
+        freq[Pattern] = 0
+        for i in range(n-k+1):
+            if Text[i:i+k] == Pattern:
+                freq[Pattern] = freq[Pattern] +1    
+           
+    return freq
+    
+Text = "ACGTTGCATGTCGCATGATGCATGAGAGCT"
+k = 4
+    
+print(FrequencyMap(Text,k))
